86c5844ed4bf8ac8e40159ca614d49a4abbecbda
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-15 19:18:20 +0300

    commit to reset tests

--- app/main.py
+++ app/main.py
@@ -7,8 +7,7 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
+    def __init__(self, distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
